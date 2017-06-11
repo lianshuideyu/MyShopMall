1734cbc48305d5133a923fad60e52c8980f5a940
lianshuideyu <834893479@qq.com>
2017-06-11 17:20:07 +0800

    主页布局实现

--- app/src/main/java/com/atguigu/myshopmall/home/fragment/HomeFragment.java
+++ app/src/main/java/com/atguigu/myshopmall/home/fragment/HomeFragment.java
@@ -1,31 +1,72 @@
 package com.atguigu.myshopmall.home.fragment;
 
-import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.atguigu.myshopmall.R;
 import com.atguigu.myshopmall.base.BaseFragment;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 /**
  * Created by Administrator on 2017/6/11.
  */
 
 public class HomeFragment extends BaseFragment {
-    private TextView tv;
+
+    @InjectView(R.id.tv_scan_home)
+    TextView tvScanHome;
+    @InjectView(R.id.tv_search_home)
+    TextView tvSearchHome;
+    @InjectView(R.id.tv_message_home)
+    TextView tvMessageHome;
+    @InjectView(R.id.rv_home)
+    RecyclerView rvHome;
+    @InjectView(R.id.ib_top)
+    ImageButton ibTop;
+
     @Override
     public View initView() {
-        tv = new TextView(context);
-        tv.setTextColor(Color.RED);
-        tv.setTextSize(30);
-        Log.e("TAG","HomeFragment--initView");
-        return tv;
+        Log.e("TAG", "HomeFragment--initView");
+        View view = View.inflate(context, R.layout.fragment_home, null);
+        ButterKnife.inject(this, view);
+        return view;
     }
 
     @Override
     public void initData() {
         super.initData();
-        tv.setText("首页");
 //        Log.e("TAG","HomeFragment--initData");
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
+    }
+
+    @OnClick({R.id.tv_scan_home, R.id.tv_search_home, R.id.tv_message_home, R.id.ib_top})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.tv_scan_home:
+                Toast.makeText(context, "扫一扫", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tv_search_home:
+                Toast.makeText(context, "搜索", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tv_message_home:
+                Toast.makeText(context, "消息", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ib_top:
+                Toast.makeText(context, "回到顶部", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
