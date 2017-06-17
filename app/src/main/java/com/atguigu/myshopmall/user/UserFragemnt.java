dd17fdef6c9d1c1afd8ba58f3253d54f2c2621b9
lianshuideyu <834893479@qq.com>
2017-06-17 18:45:08 +0800

    个人中心布局;；第三方GradationTitleBar渐变标题

--- app/src/main/java/com/atguigu/myshopmall/user/UserFragemnt.java
+++ app/src/main/java/com/atguigu/myshopmall/user/UserFragemnt.java
@@ -1,31 +1,94 @@
 package com.atguigu.myshopmall.user;
 
-import android.graphics.Color;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.atguigu.myshopmall.R;
 import com.atguigu.myshopmall.base.BaseFragment;
+import com.hankkin.gradationscroll.GradationScrollView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 /**
  * Created by Administrator on 2017/6/11.
  */
 
 public class UserFragemnt extends BaseFragment {
-    private TextView tv;
+    @InjectView(R.id.ib_user_icon_avator)
+    ImageButton ibUserIconAvator;
+    @InjectView(R.id.tv_username)
+    TextView tvUsername;
+    @InjectView(R.id.rl_header)
+    RelativeLayout rlHeader;
+    @InjectView(R.id.tv_all_order)
+    TextView tvAllOrder;
+    @InjectView(R.id.tv_user_pay)
+    TextView tvUserPay;
+    @InjectView(R.id.tv_user_receive)
+    TextView tvUserReceive;
+    @InjectView(R.id.tv_user_finish)
+    TextView tvUserFinish;
+    @InjectView(R.id.tv_user_drawback)
+    TextView tvUserDrawback;
+    @InjectView(R.id.tv_user_location)
+    TextView tvUserLocation;
+    @InjectView(R.id.tv_user_collect)
+    TextView tvUserCollect;
+    @InjectView(R.id.tv_user_coupon)
+    TextView tvUserCoupon;
+    @InjectView(R.id.tv_user_score)
+    TextView tvUserScore;
+    @InjectView(R.id.tv_user_prize)
+    TextView tvUserPrize;
+    @InjectView(R.id.tv_user_ticket)
+    TextView tvUserTicket;
+    @InjectView(R.id.tv_user_invitation)
+    TextView tvUserInvitation;
+    @InjectView(R.id.tv_user_callcenter)
+    TextView tvUserCallcenter;
+    @InjectView(R.id.tv_user_feedback)
+    TextView tvUserFeedback;
+    @InjectView(R.id.ll_root)
+    LinearLayout llRoot;
+    @InjectView(R.id.scrollview)
+    GradationScrollView scrollview;
+    @InjectView(R.id.tv_usercenter)
+    TextView tvUsercenter;
+    @InjectView(R.id.ib_user_setting)
+    ImageButton ibUserSetting;
+    @InjectView(R.id.ib_user_message)
+    ImageButton ibUserMessage;
+
     @Override
     public View initView() {
-        tv = new TextView(mContext);
-        tv.setTextColor(Color.RED);
-        tv.setTextSize(30);
-        Log.e("TAG","UserFragemnt--initView");
-        return tv;
+        Log.e("TAG", "UserFragemnt--initView");
+        View view = View.inflate(mContext, R.layout.fragment_user, null);
+        ButterKnife.inject(this, view);
+        return view;
     }
 
     @Override
     public void initData() {
-        tv.setText("用户中心");
 
         super.initData();
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
+    }
+
+    @OnClick(R.id.tv_username)
+    public void onViewClicked() {
+        Toast.makeText(mContext, "登录", Toast.LENGTH_SHORT).show();
     }
 }
