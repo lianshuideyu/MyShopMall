83d182149221a2a92537b2009fb47964a01cabf9
lianshuideyu <834893479@qq.com>
2017-06-17 19:22:53 +0800

    实现滑动标题栏颜色渐变

--- app/src/main/java/com/atguigu/myshopmall/user/UserFragemnt.java
+++ app/src/main/java/com/atguigu/myshopmall/user/UserFragemnt.java
@@ -1,7 +1,9 @@
 package com.atguigu.myshopmall.user;
 
+import android.graphics.Color;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -20,7 +22,7 @@ import butterknife.OnClick;
  * Created by Administrator on 2017/6/11.
  */
 
-public class UserFragemnt extends BaseFragment {
+public class UserFragemnt extends BaseFragment implements GradationScrollView.ScrollViewListener {
     @InjectView(R.id.ib_user_icon_avator)
     ImageButton ibUserIconAvator;
     @InjectView(R.id.tv_username)
@@ -65,12 +67,15 @@ public class UserFragemnt extends BaseFragment {
     ImageButton ibUserSetting;
     @InjectView(R.id.ib_user_message)
     ImageButton ibUserMessage;
+    private int height;
 
     @Override
     public View initView() {
         Log.e("TAG", "UserFragemnt--initView");
         View view = View.inflate(mContext, R.layout.fragment_user, null);
         ButterKnife.inject(this, view);
+
+
         return view;
     }
 
@@ -78,6 +83,57 @@ public class UserFragemnt extends BaseFragment {
     public void initData() {
 
         super.initData();
+
+        initListeners();
+        tvUsercenter.setBackgroundColor(Color.argb((int) 255, 255,0,0));
+    }
+
+    /**
+     * 获取顶部图片高度后，设置滚动监听
+     */
+    private void initListeners() {
+        tvUsercenter.setBackgroundColor(Color.argb((int) 0, 255,0,0));
+
+        ViewTreeObserver vto = rlHeader.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                tvUsercenter.getViewTreeObserver().removeGlobalOnLayoutListener(
+                        this);
+                height = rlHeader.getHeight();
+
+                scrollview.setScrollViewListener(UserFragemnt.this);
+            }
+        });
+    }
+
+    /**
+     * 滑动监听
+     * @param scrollView
+     * @param x
+     * @param y
+     * @param oldx
+     * @param oldy
+     */
+    @Override
+    public void onScrollChanged(GradationScrollView scrollView, int x, int y, int oldx, int oldy) {
+        // TODO Auto-generated method stub
+        if (y <= 0) {   //设置标题的背景颜色
+            tvUsercenter.setBackgroundColor(Color.argb((int) 0, 255,0,0));
+
+        } else if (y > 0 && y <= height) { //滑动距离小于banner图的高度时，设置背景和字体颜色颜色透明度渐变
+            float scale = (float) y / height;
+            float alpha = (255 * scale);
+            //滑动距离 ： 总距离 = 改变的透明度 ： 总透明度
+            //改变的透明度 = (滑动距离 ：总距离) *总透明度
+
+            tvUsercenter.setTextColor(Color.argb((int) alpha, 255,255,255));
+
+            tvUsercenter.setBackgroundColor(Color.argb((int) alpha, 255,0,0));//alpha,144,151,166
+
+        } else {    //滑动到banner下面设置普通颜色
+            tvUsercenter.setBackgroundColor(Color.argb((int) 255, 255,0,0));//255, 255,151,166
+        }
     }
 
 
@@ -91,4 +147,6 @@ public class UserFragemnt extends BaseFragment {
     public void onViewClicked() {
         Toast.makeText(mContext, "登录", Toast.LENGTH_SHORT).show();
     }
+
+
 }
